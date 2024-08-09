f7d74f107019dc37dde52a8a08b058a88905bffc
manash-codes <manash.codes@gmail.com>
2024-08-09 20:12:19 +0530

    Design Login Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[#081420] text-white flex min-h-screen flex-col items-center justify-center`}>{children}</body>
     </html>
   );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default async function Login() {
-    return <div>Login</div>;
+    return <div className="rounded-2xl border-4 border-slate-500 p-6">
+        <h1 className="text-center text-5xl font-bold">Login</h1>
+        <form action="">
+            <div className="my-4 flex flex-col">
+                <label className="my-2 ps-3 text-base font-bold" htmlFor="email">Email</label>
+                <input type="text" id="email" className="text-md w-[410px] rounded-lg p-4 ps-8 text-black outline-none" />
+                <label className="my-2 ps-3 text-base font-bold" htmlFor="password">Password</label>
+                <input type="password" id="password" className="text-md w-[410px] rounded-lg p-4 ps-8 text-black outline-none" />
+            </div>
+            <button className="my-4 w-full rounded-lg bg-[#2ECA34] p-4 text-xl font-bold">
+                Login
+            </button>
+        </form>
+        <Link href="/signup" className="text-center text-base font-bold">
+            <p >Not a member? SignUp</p>
+        </Link>
+    </div>;
 }
